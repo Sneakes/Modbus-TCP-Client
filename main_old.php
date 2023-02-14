e836548c1edc428322d5c62fe9950b09388778c6
Sneakes <47457425+Sneakes@users.noreply.github.com>
2023-02-14 16:10:25 +0100

    fini

--- main_old.php
+++ main_old.php
@@ -11,10 +11,48 @@ function afficher_menu()
     echo "0. Quitter\n";
 }
 
+//FC1 - Read coils
+//FC2 - Read input discretes
+//FC3 - Read holding registers
+//FC4 - Read holding input registers
+//FC5 - Write single coil
+//FC6 - Write single register
+//FC15 - Write multiple coils
+//FC16 - Write multiple registers
+//FC23 - Read/Write multiple registers
 $ip = readline("Saisissez une adresse IP: ");
 $protocol = readLine("Protocole (TCP ou UDP): ");
 $modbus = new ModbusMaster($ip, $protocol);
 system('clear');
+
+/*
+if ($this->socket_protocol == "TCP"){ 
+    $this->sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);      
+} elseif ($this->socket_protocol == "UDP"){
+    $this->sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+} else {
+    throw new Exception("Unknown socket protocol, should be 'TCP' or 'UDP'");
+}
+if (strlen($this->client)>0){
+    $result = socket_bind($this->sock, $this->client, $this->client_port);
+    if ($result === false) {
+        throw new Exception("socket_bind() failed.</br>Reason: ($result)".
+            socket_strerror(socket_last_error($this->sock)));
+    } else {
+        $this->status .= "Bound\n";
+    }
+}
+// Connect the socket
+$result = @socket_connect($this->sock, $this->host, $this->port);
+if ($result === false) {
+    throw new Exception("socket_connect() failed.</br>Reason: ($result)".
+        socket_strerror(socket_last_error($this->sock)));
+} else {
+    $this->status .= "Connected\n";
+    return true;        
+}
+sleep(2000);
+*/
 
 do
 {
@@ -100,11 +138,23 @@ do
             }
             catch(Exception $e)
             {
+                // Print error information if any
                 echo $modbus;
                 echo $e;
                 exit;
             }}
         break;
+        /*
+        case 5:
+            // code pour l'option 5
+        break;
+        case 6:
+            // code pour l'option 6
+        break;
+        case 0:
+            echo "Au revoir!\n";
+        break;
+        */
         default:
             echo "Choix invalide. Veuillez choisir une option entre 0 et 6.\n";
         break;
